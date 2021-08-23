c2c990da16e8c2cf51156d1231516704a46bb90f
Charlie Martinez <caramartinezrio@unal.edu.co>
2021-08-23 11:59:04 -0500

    Main mapping on app.js + Changes in favicon

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <>
+      <div className="top-bar">
+        <div className="top-bar__title">
+          <h1>Git Viewer</h1>
+        </div>
+      </div>
+    </>
+  );
+}
